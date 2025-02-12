c452f627d43575173e7ed419e117c27c9ab92583
NoFiringPin <tyler_boulom@yahoo.com>
2025-02-11 17:31:46 -0800

    Refactored code to protect database api key

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,24 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'splashscreen.dart';
 import 'plantselectionpage.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env"); // Initialize .env file
 
   if (kIsWeb) {
     await Firebase.initializeApp(
       options: FirebaseOptions(
-        apiKey: "AIzaSyCzYlKEe8mbF3Y-k-hw_MXs-O2qNy38uIA",
-        authDomain: "plantprojectapp.firebaseapp.com",
-        databaseURL: "https://plantprojectapp-default-rtdb.firebaseio.com",
-        projectId: "plantprojectapp",
-        storageBucket: "plantprojectapp.appspot.com",
-        messagingSenderId: "606886870127",
-        appId: "1:606886870127:web:9b5f616a7d7abf95e9a19a",
-        measurementId: "G-8VGQCD9HHH",
+        apiKey: dotenv.env['FIREBASE_API_KEY']!,
+        authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN']!,
+        databaseURL: dotenv.env['FIREBASE_DATABASE_URL']!,
+        projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+        storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+        appId: dotenv.env['FIREBASE_APP_ID']!,
+        measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID']!,
       ),
     );
   } else {
